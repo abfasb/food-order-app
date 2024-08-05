f74364e913f7c0a3b7da0451c31ec8cc9cda8f19
mtthw <matbalinton@gmail.com>
2024-08-05 21:07:31 +0800

    Add the express-validator

--- backend/middleware/validation.ts
+++ backend/middleware/validation.ts
@@ -1,2 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express";;
+import { body, validationResult } from 'express-validator';
+
+const ValidatoryRequest = (req: Request, res: Response) => {
+    const getValidate = validationResult(req);
+
+    if(!getValidate) {
+        
+    }
+}
+
+export const validatorRequest = [
+    body("name").isString().notEmpty().withMessage("Name is required"),
+    body("addressLine1").isString().notEmpty().withMessage("Address is required"),
+    body("city").isString().notEmpty().withMessage("City is required"),
+    body("country").isString().notEmpty().withMessage("Country is required"),
+];
 
