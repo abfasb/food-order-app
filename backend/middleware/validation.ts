44319da7c488879feb7d4dc5624195ee21c7be9b
mtthw <matbalinton@gmail.com>
2024-08-09 14:53:59 +0800

    Add Middleware

--- backend/middleware/validation.ts
+++ backend/middleware/validation.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from "express";;
+import { error } from "console";
+import { Request, Response, NextFunction } from "express";;
 import { body, validationResult } from 'express-validator';
 
-const ValidatoryRequest = (req: Request, res: Response) => {
+export const validateMyRequest = (req: Request, res: Response, next : NextFunction) => {
     const getValidate = validationResult(req);
 
-    if(!getValidate) {
-        
+    if(!getValidate.isEmpty()) {
+        return res.status(400).json( {errors: getValidate.array()})
     }
+    next();
 }
+
 
 export const validatorRequest = [
     body("name").isString().notEmpty().withMessage("Name is required"),
@@ -15,4 +18,28 @@ export const validatorRequest = [
     body("city").isString().notEmpty().withMessage("City is required"),
     body("country").isString().notEmpty().withMessage("Country is required"),
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("deliveryPrice")
+      .isFloat({ min: 0 })
+      .withMessage("Delivery price must be a positive number"),
+    body("estimatedDeliveryTime")
+      .isInt({ min: 0 })
+      .withMessage("Estimated delivery time must be a postivie integar"),
+    body("cuisines")
+      .isArray()
+      .withMessage("Cuisines must be an array")
+      .not()
+      .isEmpty()
+      .withMessage("Cuisines array cannot be empty"),
+    body("menuItems").isArray().withMessage("Menu items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+    body("menuItems.*.price")
+      .isFloat({ min: 0 })
+      .withMessage("Menu item price is required and must be a postive number"),
+    validateMyRequest,
+]
 
--- backend/routes/myRestaurant.ts
+++ backend/routes/myRestaurant.ts
@@ -1,12 +1,23 @@
 import MyRestaurantController from '../controllers/MyRestaurantController';
+import multer from 'multer';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyRestaurantRequest } from '../middleware/validation';
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    }
+})
 
 const express = require('express');
 
 const router = express.Router();
 
-router.post('/', MyRestaurantController.createCurrentUser);
-router.post('/', MyRestaurantController.getCurrentUser);
-router.post('/', MyRestaurantController.updateCurrentUser);
+router.post('/', upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, MyRestaurantController.createCurrentUser);
+router.get('/', jwtCheck, jwtParse, MyRestaurantController.getCurrentUser);
+router.put('/', jwtCheck, jwtParse, MyRestaurantController.updateCurrentUser);
 
 export default router;
 
